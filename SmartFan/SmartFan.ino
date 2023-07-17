ae20a453214bdcd8dc66d8a6a28be1d866a74803
ParkMini <47918357+ParkMini@users.noreply.github.com>
2023-07-18 08:20:08 +0900

    주석 추가

--- SmartFan/SmartFan.ino
+++ SmartFan/SmartFan.ino
@@ -1,11 +1,12 @@
-int SPEED_BTN = 7;
-int EMERGENCY_BTN = 8;
+// 아두이노 나노 코드
+int SPEED_BTN = 7; // D7
+int EMERGENCY_BTN = 8; // D8
 int EMERGENCY_LED = 9; // D9 (PWM)
 int PWM_PIN = 10; // D10 (PWM)
 int IN1_PIN = 11; // D11
 int IN2_PIN = 12; // D12
-int SPEED = 0;
-int EMERGENCY_STOP = 0;
+int SPEED = 0; // 모터 속도 (0~4단)
+int EMERGENCY_STOP = 0; // 긴급 정지 (0 또는 1)
 
 void setup()
 {
@@ -17,6 +18,7 @@ void setup()
   pinMode(EMERGENCY_LED, OUTPUT);
   pinMode(SPEED_BTN, INPUT_PULLUP);
   pinMode(EMERGENCY_BTN, INPUT_PULLUP);
+  // 밑과 같이 값을 넣어줘야 모터가 정방향으로 회전
   digitalWrite(IN1_PIN, HIGH);
   digitalWrite(IN2_PIN, LOW);
 }
@@ -25,6 +27,8 @@ void loop()
 {
   int SPEED_BTN_VALUE = digitalRead(SPEED_BTN);
   int EMERGENCY_BTN_VALUE = digitalRead(EMERGENCY_BTN);
+
+  // 모터 속도 제어
   if (SPEED_BTN_VALUE == LOW && EMERGENCY_STOP == 0) {
     SPEED += 1;
     if (SPEED == 1) {
@@ -42,6 +46,7 @@ void loop()
     delay(100);
   }
 
+  // 긴급 정지
   if (EMERGENCY_BTN_VALUE == LOW) {
     if (EMERGENCY_STOP == 0) {
     EMERGENCY_STOP = 1;
